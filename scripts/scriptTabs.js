c0d77527a0c577ebd05ea565d7da2089f8c74a68
Bruno Nóbrega <105138577+nobrnbruno@users.noreply.github.com>
2023-05-29 03:27:19 -0300

    updates on the tabs

--- scripts/scriptTabs.js
+++ scripts/scriptTabs.js
@@ -5,11 +5,6 @@ function showPanel(panelIndex,colorCode) {
     tabButtons.forEach(function(node){
         node.style.backgroundColor="";
         node.style.color="";
-<<<<<<< Updated upstream
-    });
-    tabButtons[panelIndex].style.backgroundColor=colorCode;
-    tabButtons[panelIndex].style.color="white";
-=======
         node.style.borderBottom="solid 2px black";
         node.style.borderTop="solid 2px black";
         node.style.borderRight="solid 2px black";
@@ -17,17 +12,14 @@ function showPanel(panelIndex,colorCode) {
     tabButtons[panelIndex].style.backgroundColor=colorCode;
     tabButtons[panelIndex].style.color="white";
     tabButtons[panelIndex].style.borderBottom="none";
->>>>>>> Stashed changes
     tabPanels.forEach(function(node){
         node.style.display="none";
     });
     tabPanels[panelIndex].style.display="block";
     tabPanels[panelIndex].style.backgroundColor=colorCode;
-<<<<<<< Updated upstream
-=======
     tabPanels[panelIndex].style.borderBottom="solid 2px black";
     tabPanels[panelIndex].style.borderLeft="solid 2px black";
     tabPanels[panelIndex].style.borderRight="solid 2px black";
->>>>>>> Stashed changes
 }
+
 showPanel(0,'#f44336');
