84267483ce01056f25314af2c4933dded10005b9
Bruno Nóbrega <105138577+nobrnbruno@users.noreply.github.com>
2023-07-03 07:19:03 -0300

    updates nas optativas e no layout de checkboxes

--- js/scriptDisciplinas.js
+++ js/scriptDisciplinas.js
@@ -20,60 +20,104 @@ function getAll() {
                 if (course.natureza == "OBRIGATÓRIA") {
                     if (course.semestre == 1) {
                         outFirst[0].innerHTML = outFirst[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 2) {
                         outSecond[0].innerHTML = outSecond[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 3) {
                         outThird[0].innerHTML = outThird[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 4) {
                         outFourth[0].innerHTML = outFourth[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 5) {
                         outFifth[0].innerHTML = outFifth[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 6) {
                         outSixth[0].innerHTML = outSixth[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 7) {
                         outSeventh[0].innerHTML = outSeventh[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 8) {
                         outEighth[0].innerHTML = outEighth[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 9) {
                         outNinth[0].innerHTML = outNinth[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                 }
                 if (course.natureza == "ELETIVA") {
                     if (course.semestre == 4) {
                         outElectivesFourth[0].innerHTML = outElectivesFourth[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                     if (course.semestre == 5) {
                         outElectivesFifth[0].innerHTML = outElectivesFifth[0].innerHTML + 
-                        `<input class="form-check-input" type="checkbox" id="flexCheckDefault" name=${course.componente} value=${course.cargahoraria} />
-                        <label for="flexCheckDefault">${course.componente}</label><br>`;
+                        `<div class="checkbox-container" style="position: relative; display: block; margin-right: 10%;">
+                            <input style="display: inline-block; position: absolute" class="form-check-input" type="checkbox" id=${course.codigo} name=${course.componente} value=${course.cargahoraria} />
+                            <span style="display: inline; position: relative; left: 1.5rem; top: 2px" class="checkbox_label">
+                               <label for=${course.codigo}>${course.componente}</label>
+                            </span>
+                        </div>`;
                     }
                 }
             });
