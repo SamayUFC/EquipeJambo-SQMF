0adcb150052dcc3bd4b9a319f0143284110b889e
Bruno Nóbrega <105138577+nobrnbruno@users.noreply.github.com>
2023-07-16 02:27:21 -0300

    Updates

--- js/scriptActivities.js
+++ js/scriptActivities.js
@@ -1,9 +1,15 @@
 function findTotal(){
-    var arrayHours = document.getElementsByClassName('inputNumberHours');
-    var totalActivities = 0;
+    let arrayHours = document.getElementsByClassName('inputNumberHours');
+    totalActivities = 0;
     for(var i=0;i<arrayHours.length;i++){
         if(parseInt(arrayHours[i].value) >= 0 && parseInt(arrayHours[i].value) <= parseInt(arrayHours[i].max))
         totalActivities += parseInt(arrayHours[i].value);
+        if (totalActivities > 192) {
+            // if (!arrayHours[i].value) {
+            //     arrayHours[i].readOnly = true;
+            // }
+            totalActivities = 192;
+        }
     }
     document.getElementById('sumActivities').innerHTML = totalActivities;
 }
